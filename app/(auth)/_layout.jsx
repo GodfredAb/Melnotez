82e933f7b95790396c9596315a2eb0ad8b87128b
AbGod <aboragodfred2@gmail.com>
2025-06-17 02:22:59 +0000

    initial commit

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Redirect, Stack } from 'expo-router'
+import { StatusBar } from "expo-status-bar";
+
+
+const AuthLayout = () => {
+    return (
+        <>
+            <Stack>
+                <Stack.Screen
+                name='SignUp'
+                options={{
+                    headerShown: false,
+                }}
+                />
+                <Stack.Screen 
+                name='SignIn'
+                options={{
+                    headerShown: false,
+                }}
+                />
+            </Stack>
+        </>
+    )
+}
+
+export default AuthLayout
+
+const styles = StyleSheet.create({})
